732b3b144cbaaa470ae93d69bc82e34cf65828a7
BlokNet <bloknett@gmail.com>
2026-07-21 16:23:40 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 // свежие версии файлов из ASSETS.
 // ============================================================
 
-const CACHE_VERSION = 'kipia-test-v120';
+const CACHE_VERSION = 'kipia-test-v121';
 const CACHE_NAME = CACHE_VERSION;
 
 // Отдельный кэш для картинок Google Drive (превью + полные).
