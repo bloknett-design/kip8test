60dc357fe848567186cae28dd5c83a9491956561
Z User <z@container>
2026-08-14 00:32:36 +0000

    Расходомеры хозрасчётные: раздел в Документация ИОС, форма ввода показаний (v320)

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 // свежие версии файлов из ASSETS.
 // ============================================================
 
-const CACHE_VERSION = 'kipia-test-v319';
+const CACHE_VERSION = 'kipia-test-v320';
 const CACHE_NAME = CACHE_VERSION;
 
 // Отдельный кэш для картинок Google Drive (превью + полные).
